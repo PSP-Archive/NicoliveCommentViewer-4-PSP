faffc22349767e94289c68c9b1a38b1ceeaa5a0d
yreeen <yreeen@e02e5e97-58c4-4a71-a283-e590bbc28360>
2009-12-28 05:15:28 +0000

    一旦消去@yreeen

--- ncps/branches/yreeen/Libs/GUDiag_osk.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GUDIAG_OSK_DECL
-#define _GUDIAG_OSK_DECL
-
-int GetOSKText(char *buf, int buflen, int inputmode, const char *title, const char *init);
-
-#endif
--- ncps/branches/yreeen/dialogs.h
+++
@@ -1,4 +0,0 @@
-
-void LoginDialog(char *mail_init, char *pass_init, char *mail, char *pass);
-
-
